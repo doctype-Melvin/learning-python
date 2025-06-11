decef84dc774465af2d8a00e9a970361cf712017
Melvin Speckamp <doctype.melvin@gmail.com>
2025-06-11 10:53:08 +0200

    point past sticking point - hash complete - start print_mimic next

--- google-python-exercises/basic/mimic.py
+++ google-python-exercises/basic/mimic.py
@@ -47,24 +47,43 @@ import sys
 
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
-  read_file = open(filename, encoding='utf-8')
   hash = {}
   giant_string = []
-
+  
+  read_file = open(filename, encoding='utf-8')
   full_string = read_file.read()
   full_split = full_string.split()
 
   for string in full_split:
     giant_string.append(string.lower())
 
-  print(giant_string)
+  i = 0
+  # this is going in the right direction
+  # iterate of the giant_string and look
+  # for each word and the next one
+  # if the word is not in the hash, add the word as key
+  # and push the next word as list item
+  while i < len(giant_string):
+    if (i+1 < len(giant_string)):
+      if not giant_string[i] in hash:
+        hash[giant_string[i]] = [giant_string[i+1]]
+        i += 1
+      else:
+        hash[giant_string[i]].append(giant_string[i+1])
+        i += 1
+    else:
+      i += 1
+  print(i)
 
-
+  """
   for ele in giant_string:
     if not ele in hash:
       hash[ele] = []
-
-
+      i += 1
+      print(i)
+    else:
+      i += 1
+  """
 
   print(hash)
   read_file.close()
